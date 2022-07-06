a95bf4c8eb3d244ccfdaf18bbb60cbf87428ac3d
Kelly Millward <kelly.millward@education.gov.uk>
2022-07-06 08:27:25 +0100

    landing page set up
    current landing page set up

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'UFS Prototype',
+  serviceName: 'Unit for Future Skills',
 
   // Default port that prototype runs on
   port: '3000',
